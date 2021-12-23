24ca6c2cfe6b1e48a616c01ab1e86ac3397f3786
Daniel Diehl <daniel.conde.diehl@adp.com>
2021-12-23 00:49:50 -0500

    Adjusting packager versioning

--- osx-packager/package-app.sh
+++ osx-packager/package-app.sh
@@ -10,16 +10,22 @@ echo "Building app.."
 cd ..
 ./mvnw package
 
+#retrieve version
+VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout|awk -F'-SNAPSHOT' '{print $1}')
+echo "Version will be: $VERSION"
+
 echo "Packaging DMG.." 
 cd -
 jpackage --type dmg \
          -i ../MSGViewer/target \
          -n "MSG Viewer" \
+         --app-version $VERSION \
          --icon "./msg-viewer.icns" \
          --main-class net.sourceforge.MSGViewer.ModuleLauncher \
          --main-jar msgviewer.jar
 
 echo "Moving to distribution folder.."
+rm -rf ../dist
 mkdir ../dist
 mv *.dmg ../dist
 
